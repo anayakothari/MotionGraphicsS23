e6d5786e39cae073922303dc4f6a81b42cb24d2b
anayakothari <anayakothari77@gmail.com>
2023-04-20 15:17:23 -0400

    heroupdates

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -3,7 +3,27 @@ import {
 } from "gsap";
 
 gsap.from("#logo", {
-    duration: 2,
+    duration: 1.5,
     alpha: 0,
     delay: 0.5
+});
+
+gsap.from("nav li", {
+    duration: 2,
+    alpha: 0,
+    stagger: .4,
+    y: "-=20",
+    delay: 1
+});
+
+gsap.from(".h2", {
+    duration: 2,
+    y: "-=20",
+    delay: .5
+});
+
+gsap.to("#hero", {
+    duration: 2,
+    y: "-=20",
+    delay: 2.5
 });
