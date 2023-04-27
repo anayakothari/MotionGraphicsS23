5c12ca55d775df26a425e5cae306e3fed60ab463
anayakothari <anayakothari77@gmail.com>
2023-04-27 17:50:21 -0400

    timelines

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,61 +1,84 @@
-import {
-    gsap
-} from "gsap";
+import {gsap} from "gsap";
 
 let mainTl = gsap.timeline({delay:1});
 
+function logoAni(){
+
+  let tl = gsap.timeline();
+  let mm = gsap.matchMedia();
+
+  mm.add("(max-width:767px)", () =>{
+  tl.from("#logo", {duration:.5, rotation: 360})
+  });
+
+  mm.add("(min-width:768px)", () =>{
+    tl.from("#logo", {duration: .5, rotation: 360})
+    });
 
 
-function animate() {
-    let tl = gsap.timeline({paused: false});
+  return tl;
+}
+
+mainTl.add(logoAni())
+
+mainTl.from("nav li", {
+  duration: .5,
+  alpha: 0,
+  stagger: {each:0.1},
+  from:"start",
+  y: "-=20"
+  //delay: .5
+})
+.from(".h2", {
+  duration: 1,
+  y: "+=30"
+  //delay: .5
+})
+.from("#button", {
+  duration: 1,
+  x: "-=1000"
+  //delay: .5
+})
+.to("#hero", {
+  duration: 1.5,
+  y: "-=20"
+})
+.to("#content", {
+  duration: 1.5,
+  //x: "-=1100",
+  rotation: 360
+  //delay: .5
+});
+
+
+
+function heroAni(){
+
+    let tl = gsap.timeline({delay:1});
     let mm = gsap.matchMedia();
 
-    
-    tl.from("#logo", {
-      duration: 1,
-      alpha: 0,
-      //delay: .5
-    })
-    .from("nav li", {
-      duration: .5,
-      alpha: 0,
-      stagger: {each:0.1},
-      from:"start",
-      y: "-=20",
-      //delay: .5
-    })
-    .from(".h2", {
-      duration: 1,
-      y: "+=30",
-      //delay: .5
-    })
-    .from("#button", {
-      duration: 1,
-      x: "-=1000",
-      //delay: .5
-    })
-    .to("#hero", {
-      duration: 2,
-      y: "-=20",
-      //delay: 1.5
-    })
-    .to("#content", {
-      duration: 1.5,
-      easing: 'ease-in-out',
-      x: "-=1100",
-      //rotation: 360,
-      //delay: .5
+    mm.add("(max-width:767px)", () =>{
+    tl.to("#hero", {duration: 1.5, y: "-=20"})
     });
-  
-    return mainTl;
-  }
-  
-mainTimeline = animate();
 
-  mainTl.add("#logo")
-  .add("#hero")
-  .add("#button")
-  .add("nav li")
-  .add(".h2")
-  .add("#content")
+    mm.add("(min-width:768px)", () =>{
+    tl.to("#hero", {duration: 1.5, y: "-=20"})
+    });
+
+
+    return tl;
+}
+
+mainTl.add(heroAni())
+;
+
+
+
   
+
+  
+  
+
+  //mainTl.add(logoAni())
+  //.add(heroAni())
+  //;
