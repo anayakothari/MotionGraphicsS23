707620ce610690264c384f1ed8fa486e92b85696
anayakothari <anayakothari77@gmail.com>
2023-04-27 20:36:08 -0400

    easing

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -5,5 +5,5 @@ import { gsap } from "gsap";
 //PART 1
 let tl = gsap.timeline();
 
-tl.from("box red", {duration:1, x: 0, ease: 1})
-.from("box blue", {duration:1, x: 350, ease: 1})
+tl.from("box red", {duration:1, x: 0, ease:"power4.easeOut"})
+.from("box blue", {duration:1, x: 350, ease: "power4.easeOut"})
