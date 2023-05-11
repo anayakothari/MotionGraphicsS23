ac773d6c788777637cca6f72d7510c4903aa25c4
anayakothari <anayakothari77@gmail.com>
2023-05-11 00:55:26 -0400

    box

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -3,13 +3,37 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.from("#box",{duration:2, scrollTrigger:{
-    trigger:"#box",
-    toggleActions:"play pause resume reverse",
-    start:"bottom 70%",
-    end:"top 30%",
-    markers:true
-    },
-    scale:2, 
-    rotation:180, 
-    x:"-=600%"});
+//gsap.from("#box",{duration:2, scrollTrigger:{
+    //trigger:"#box",
+    //toggleActions:"play none none reverse",
+    //start:"bottom 70%",
+    //end:"top 30%",
+    //markers:true
+    //},
+    //scale:2, 
+    //rotation:360, 
+    //x:"-=600%"});
+
+function boxAnimation(){
+    let tl = gsap.timeline({
+        scrollTrigger:{
+            trigger: "#box",
+            toggleActions: "play none none reverse",
+            start:"bottom 70%",
+            end:"top 30%",
+            markers:true
+         }});
+
+    tl.from("#box", {duration:2, scale:2, rotation:360, x:"-=600%"})
+    ;
+
+    return tl;
+
+}
+
+let mainTl = gsap.timeline();
+mainTl.add(boxAnimation());
+    //.add(boxAnimation())
+    //.add(hikingAnimation())
+    //.add(hero2Animation())
+    
