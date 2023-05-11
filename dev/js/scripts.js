f73160cfbb59616a71972c24d785075c40fb9c28
anayakothari <anayakothari77@gmail.com>
2023-05-11 00:10:35 -0400

    update

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -3,3 +3,13 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
+gsap.from("#box",{duration:2, scrollTrigger:{
+    trigger:"#box",
+    toggleActions:"play pause resume reverse",
+    start:"bottom 70%",
+    end:"top 30%",
+    markers:true
+    },
+    scale:2, 
+    rotation:180, 
+    x:"-=600%"});
