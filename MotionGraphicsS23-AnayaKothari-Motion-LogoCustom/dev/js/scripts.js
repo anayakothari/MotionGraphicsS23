49b5eb68bca2bdf8e5b7332132ef77c5b9ebd43e
anayakothari <anayakothari77@gmail.com>
2023-06-08 00:49:35 -0400

    update

--- MotionGraphicsS23-AnayaKothari-Motion-LogoCustom/dev/js/scripts.js
+++ MotionGraphicsS23-AnayaKothari-Motion-LogoCustom/dev/js/scripts.js
@@ -1,10 +1,14 @@
 import { gsap } from "gsap";
 import { MotionPathPlugin } from "gsap/MotionPathPlugin";
+import { TextPlugin } from "gsap/TextPlugin";
+import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
 import { GSDevTools } from "gsap/GSDevTools";
 
-gsap.registerPlugin(MotionPathPlugin, GSDevTools);
+gsap.registerPlugin(MotionPathPlugin, TextPlugin, DrawSVGPlugin, GSDevTools);
 
 const mainTL = gsap.timeline();
+
+mainTL.from("#logomark", {});
 
 
 
