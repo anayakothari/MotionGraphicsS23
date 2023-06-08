77a884ef6f6646dcd3505818cfe6fd1c738a5ad7
anayakothari <anayakothari77@gmail.com>
2023-06-08 15:51:02 -0400

    .

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,1 +1,21 @@
-//import { gsap } from "gsap";
+import { gsap } from "gsap";
+import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
+import { GSDevTools } from "gsap/GSDevTools";
+
+gsap.registerPlugin(MorphSVGPlugin, GSDevTools);
+
+const mainTL = gsap.timeline();
+
+
+
+mainTL.to("#firstsquare",{duration:.8,scale: 1})
+.to("#firstsquare",{duration:.8, morphSVG:"#breakfour", scale: .8, fill: "#DF6436", stroke: "#DF6436"})
+.to("#firstsquare",{duration:.8, morphSVG:"#squareformation"})
+.to("#firstsquare",{duration:.8, morphSVG:"#squareformgrow"})
+.to("#firstsquare",{duration:.8, morphSVG:"#finallogo"})
+.to("#s",{duration: 0.6, opacity: 1},'-=0.2')
+
+;
+
+GSDevTools.create();
+
