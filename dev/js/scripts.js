649d2a3143fe04b4493ec7983afd21b9f8fbfe59
anayakothari <anayakothari77@gmail.com>
2023-06-08 16:25:02 -0400

    final

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -9,17 +9,17 @@ gsap.registerPlugin(MotionPathPlugin, TextPlugin, DrawSVGPlugin, GSDevTools);
 const mainTL = gsap.timeline();
 
 mainTL.fromTo("#musicschool", {duration:1, y: '700%'},{duration: 2, y: '0%', ease: 'bounce.out'})
-.to("#s", {duration:.15, y: -10, ease: 'bounce.out', yoyo: true, repeat: 1})
-.to("#e", {duration:.15, y: -10, ease: 'bounce.out', yoyo: true, repeat: 1})
-.to("#t1", {duration:.15, y: -10, ease: 'bounce.out', yoyo: true, repeat: 1})
-.to("#t2", {duration:.15, y: -10, ease: 'bounce.out', yoyo: true, repeat: 1})
-.to("#l", {duration:.15, y: -10, ease: 'bounce.out', yoyo: true, repeat: 1})
-.to("#e2", {duration:.15, y: -10, ease: 'bounce.out', yoyo: true, repeat: 1})
-.to("#m", {duration:.15, y: -10, ease: 'bounce.out', yoyo: true, repeat: 1})
-.to("#e3", {duration:.15, y: -10, ease: 'bounce.out', yoyo: true, repeat: 1})
-.to("#n", {duration:.15, y: -10, ease: 'bounce.out', yoyo: true, repeat: 1})
-.to("#t3", {duration:.15, y: -10, ease: 'bounce.out', yoyo: true, repeat: 1})
-.from("#logomark", {duration:2,x:"-=1000%", rotation: 180})
+.to("#s", {duration:.1, y: -10, ease: 'bounce.out', yoyo: true, repeat: 1})
+.to("#e", {duration:.1, y: -10, ease: 'bounce.out', yoyo: true, repeat: 1})
+.to("#t1", {duration:.1, y: -10, ease: 'bounce.out', yoyo: true, repeat: 1})
+.to("#t2", {duration:.1, y: -10, ease: 'bounce.out', yoyo: true, repeat: 1})
+.to("#l", {duration:.1, y: -10, ease: 'bounce.out', yoyo: true, repeat: 1})
+.to("#e2", {duration:.1, y: -10, ease: 'bounce.out', yoyo: true, repeat: 1})
+.to("#m", {duration:.1, y: -10, ease: 'bounce.out', yoyo: true, repeat: 1})
+.to("#e3", {duration:.1, y: -10, ease: 'bounce.out', yoyo: true, repeat: 1})
+.to("#n", {duration:.1, y: -10, ease: 'bounce.out', yoyo: true, repeat: 1})
+.to("#t3", {duration:.1, y: -10, ease: 'bounce.out', yoyo: true, repeat: 1})
+.from("#logomark", {duration:2,x:"-=400%", y: -10, ease: 'bounce.out', yoyo: true})
 
 
 ;
