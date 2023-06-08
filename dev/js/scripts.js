38cccffbca9cac7e6b0dbb9e02a08d1aa1862263
anayakothari <anayakothari77@gmail.com>
2023-06-08 01:44:03 -0400

    .

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -8,7 +8,21 @@ gsap.registerPlugin(MotionPathPlugin, TextPlugin, DrawSVGPlugin, GSDevTools);
 
 const mainTL = gsap.timeline();
 
-mainTL.from("#logomark", {duration:3});
+mainTL.to("#s", {duration:.15, y: -10, ease: 'bounce.out', yoyo: true, repeat: 1})
+.to("#e", {duration:.15, y: -10, ease: 'bounce.out', yoyo: true, repeat: 1 })
+.to("#t1", {duration:.15, y: -10, ease: 'bounce.out', yoyo: true, repeat: 1 })
+.to("#t2", {duration:.15, y: -10, ease: 'bounce.out', yoyo: true, repeat: 1 })
+.to("#l", {duration:.15, y: -10, ease: 'bounce.out', yoyo: true, repeat: 1 })
+.to("#e2", {duration:.15, y: -10, ease: 'bounce.out', yoyo: true, repeat: 1 })
+.to("#m", {duration:.15, y: -10, ease: 'bounce.out', yoyo: true, repeat: 1 })
+.to("#e3", {duration:.15, y: -10, ease: 'bounce.out', yoyo: true, repeat: 1 })
+.to("#n", {duration:.15, y: -10, ease: 'bounce.out', yoyo: true, repeat: 1 })
+.to("#t3", {duration:.15, y: -10, ease: 'bounce.out', yoyo: true, repeat: 1 })
+.fromTo("#musicschool", {duration:1, x: '-400%'},{duration: 2, x: '0%', ease: 'bounce.out'})
+.from("#logomark", {duration:2,x:"-=1000%", rotation: 360})
+
+
+;
 
 
 
