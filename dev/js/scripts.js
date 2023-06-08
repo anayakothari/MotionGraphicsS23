c2fcb3a8da073b3cec1993beb8eb6d8e70175024
anayakothari <anayakothari77@gmail.com>
2023-06-08 01:55:12 -0400

    .

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -8,18 +8,18 @@ gsap.registerPlugin(MotionPathPlugin, TextPlugin, DrawSVGPlugin, GSDevTools);
 
 const mainTL = gsap.timeline();
 
-mainTL.to("#s", {duration:.15, y: -10, ease: 'bounce.out', yoyo: true, repeat: 1})
-.to("#e", {duration:.15, y: -10, ease: 'bounce.out', yoyo: true, repeat: 1 })
-.to("#t1", {duration:.15, y: -10, ease: 'bounce.out', yoyo: true, repeat: 1 })
-.to("#t2", {duration:.15, y: -10, ease: 'bounce.out', yoyo: true, repeat: 1 })
-.to("#l", {duration:.15, y: -10, ease: 'bounce.out', yoyo: true, repeat: 1 })
-.to("#e2", {duration:.15, y: -10, ease: 'bounce.out', yoyo: true, repeat: 1 })
-.to("#m", {duration:.15, y: -10, ease: 'bounce.out', yoyo: true, repeat: 1 })
-.to("#e3", {duration:.15, y: -10, ease: 'bounce.out', yoyo: true, repeat: 1 })
-.to("#n", {duration:.15, y: -10, ease: 'bounce.out', yoyo: true, repeat: 1 })
-.to("#t3", {duration:.15, y: -10, ease: 'bounce.out', yoyo: true, repeat: 1 })
-.fromTo("#musicschool", {duration:1, x: '-400%'},{duration: 2, x: '0%', ease: 'bounce.out'})
-.from("#logomark", {duration:2,x:"-=1000%", rotation: 360})
+mainTL.fromTo("#musicschool", {duration:1, y: '700%'},{duration: 2, y: '0%', ease: 'bounce.out'})
+.to("#s", {duration:.15, y: -10, ease: 'bounce.out', yoyo: true, repeat: 1})
+.to("#e", {duration:.15, y: -10, ease: 'bounce.out', yoyo: true, repeat: 1})
+.to("#t1", {duration:.15, y: -10, ease: 'bounce.out', yoyo: true, repeat: 1})
+.to("#t2", {duration:.15, y: -10, ease: 'bounce.out', yoyo: true, repeat: 1})
+.to("#l", {duration:.15, y: -10, ease: 'bounce.out', yoyo: true, repeat: 1})
+.to("#e2", {duration:.15, y: -10, ease: 'bounce.out', yoyo: true, repeat: 1})
+.to("#m", {duration:.15, y: -10, ease: 'bounce.out', yoyo: true, repeat: 1})
+.to("#e3", {duration:.15, y: -10, ease: 'bounce.out', yoyo: true, repeat: 1})
+.to("#n", {duration:.15, y: -10, ease: 'bounce.out', yoyo: true, repeat: 1})
+.to("#t3", {duration:.15, y: -10, ease: 'bounce.out', yoyo: true, repeat: 1})
+.from("#logomark", {duration:2,x:"-=1000%", rotation: 180})
 
 
 ;
