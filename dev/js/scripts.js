3e3a2577141b48a0421efb6897a4a104aba23c30
anayakothari <anayakothari77@gmail.com>
2023-06-14 21:01:26 -0400

    final

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,14 +1,17 @@
 import { gsap } from "gsap";
-import { MotionPathPlugin } from "gsap/MotionPathPlugin";
+import { EasePack } from "gsap/EasePack";
+import { Physics2DPlugin } from "gsap/Physics2DPlugin";
 import { TextPlugin } from "gsap/TextPlugin";
 import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
 import { GSDevTools } from "gsap/GSDevTools";
 
-gsap.registerPlugin(MotionPathPlugin, TextPlugin, DrawSVGPlugin, GSDevTools);
+gsap.registerPlugin(EasePack, TextPlugin, DrawSVGPlugin, Physics2DPlugin, GSDevTools);
 
 const mainTL = gsap.timeline();
 
-mainTL.from("#logomark", {duration:1.5,x:"+=300%", ease: 'elastic', yoyo: true})
+mainTL.to("#logomark", {duration: 0.5, physics2D: {velocity: 100, angle: 0}})
+.to("#logomark", {duration: 0.5, physics2D: {gravity: 0}})
+.from("#logomark", {duration:1.5,x:"-=300%", ease: 'elastic', yoyo: true})
 .fromTo(["#s", "#e", "#t1", "#t2", "#l", "#e2", "#m", "#e3", "#n", "#t3"], {duration:1, y: '-800%'},{duration: 1, y: '0%', ease: 'elastic'})
 .to("#s", {duration:.1, y: -10, ease: 'bounce.out', yoyo: true, repeat: 1})
 .to("#e", {duration:.1, y: -10, ease: 'bounce.out', yoyo: true, repeat: 1})
